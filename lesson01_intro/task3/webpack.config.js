d445a7a0201f254e4b9f7ffe7a4df53fbdc08cfe
yuriiburov <burovyuriireact@gmail.com>
2022-01-08 16:42:50 +0200

    init

--- lesson01_intro/task3/webpack.config.js
+++ lesson01_intro/task3/webpack.config.js
@@ -1,6 +1,9 @@
 const HTMLWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
 
 module.exports = {
+  entry: './src/index.js',
+  output: { path: path.resolve(__dirname, 'review_build') },
   module: {
     rules: [
       {
