f22138bbcd9f26bd2b12f989cd0c195dca9c1671
yuriiburov <burovyuriireact@gmail.com>
2022-01-08 18:31:30 +0200

    init

--- lesson01_intro/task4/src/index.js
+++ lesson01_intro/task4/src/index.js
@@ -2,13 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-const rootElement = document.querySelector('#root');
-
-const element = (
-  <div className="greeting">
-    <div className="greeting__title">Hello, world!</div>
-    <div className="greeting__text">I'm learning React</div>
-  </div>
+ReactDOM.render(
+  <h1 class="title">Todo List</h1>,
+  document.querySelector('#root')
 );
-
-ReactDOM.render(element, rootElement);
