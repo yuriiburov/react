ff71474f3d0c3d67c010b92151aafa07f0b90a55
yuriiburov <burovyuriireact@gmail.com>
2022-01-08 22:23:09 +0200

    init

---
+++ lesson02_JSX/task4/src/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+const element = (
+  <>
+    <h1 class="title">Todo List</h1>
+    <main class="todo-list">
+      <div class="actions">
+        <input class="task-input" type="text" />
+        <button class="btn create-task-btn">Create</button>
+      </div>
+      <ul class="list"></ul>
+    </main>
+  </>
+);
+
+ReactDOM.render(element, document.getElementById('root'));
