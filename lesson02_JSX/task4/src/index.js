c2b8932bc003020e121020ecd89418f4bef6cca1
yuriiburov <burovyuriireact@gmail.com>
2022-01-08 22:29:38 +0200

    init

--- lesson02_JSX/task4/src/index.js
+++ lesson02_JSX/task4/src/index.js
@@ -4,13 +4,13 @@ import './index.css';
 
 const element = (
   <>
-    <h1 class="title">Todo List</h1>
-    <main class="todo-list">
-      <div class="actions">
-        <input class="task-input" type="text" />
-        <button class="btn create-task-btn">Create</button>
+    <h1 className="title">Todo List</h1>
+    <main className="todo-list">
+      <div className="actions">
+        <input className="task-input" type="text" />
+        <button className="btn create-task-btn">Create</button>
       </div>
-      <ul class="list"></ul>
+      <ul className="list"></ul>
     </main>
   </>
 );
