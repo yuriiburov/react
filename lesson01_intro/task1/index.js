4a8cdcbeec0accaa00f3baff36c38ff15829d771
yuriiburov <burovyuriireact@gmail.com>
2022-01-08 16:23:52 +0200

    init

--- lesson01_intro/task1/index.js
+++ lesson01_intro/task1/index.js
@@ -1,16 +1,9 @@
-// <div class="greeting">Hello, React!</div>
+// const rootElement = document.querySelector('#root');
 
-const rootElement = document.querySelector('#root');
-// const greetingElem = document.createElement('div');
-// greetingElem.classList.add('greeting');
-// greetingElem.textContent = 'Hello, React!';
+// const greetingElem = React.createElement(
+//   'div',
+//   { className: 'greeting' },
+//   'Hello, React!'
+// );
 
-// rootElement.append(greetingElem);
-
-const greetingElem = React.createElement(
-  'div',
-  { className: 'greeting' },
-  'Hello, React!'
-);
-
-ReactDOM.render(greetingElem, rootElement);
+// ReactDOM.render(greetingElem, rootElement);
