0565f56585c2690733d1ce397fe4b83329c502a4
yuriiburov <burovyuriireact@gmail.com>
2022-01-10 21:43:38 +0200

    init

--- lesson03_components/task3/src/index.jsx
+++ lesson03_components/task3/src/index.jsx
@@ -1,6 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './styles.css';
-import App from './App.jsx';
+import Comment from './Comment';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const userInfo = {
+  name: 'Tom',
+  avatarUrl: 'https://avatars1.githubusercontent.com',
+};
+
+ReactDOM.render(
+  <Comment
+    user={userInfo}
+    text="Good job!"
+    date={new Date('2019-01-01T11:32:19.566Z')}
+  />,
+  document.getElementById('root')
+);
