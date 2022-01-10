c7aaf178ccbe1c0243be0b0e87b399ac7982899a
yuriiburov <burovyuriireact@gmail.com>
2022-01-10 20:46:22 +0200

    init

---
+++ lesson03_components/task2/src/Search.jsx
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+import './search.css';
+
+// const Search = (props) => {
+//   return (
+//     <div className="search">
+//       <h1 className="search__title">{`Hello, ${props.name}. What to search for you?`}</h1>
+//       <div className="search__field">
+//         <input type="text" className="search__input" />
+//         <button className="search__button">Search</button>
+//       </div>
+//     </div>
+//   );
+// };
+
+class Search extends Component {
+  render() {
+    return (
+      <div className="search">
+        <h1 className="search__title">{`Hello, ${this.props.name}. What to search for you?`}</h1>
+        <div className="search__field">
+          <input type="text" className="search__input" />
+          <button className="search__button">Search</button>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Search;
---
+++ lesson03_components/task3/src/Comment.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import './comment.css';
+import moment from 'moment';
+import UserInfo from './UserInfo'; //
+
+const formatDate = (date) => moment(date).format('DD MMM YYYY');
+
+function Comment(props) {
+  return (
+    <div className="comment">
+      <UserInfo user={props.user} />
+      <div className="comment__text">{props.text}</div>
+      <div className="comment__date">{formatDate(props.date)}</div>
+    </div>
+  );
+}
+
+export default Comment;
