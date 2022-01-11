afce90db3dbe3412bcb00dc8caffdebad1e81dc8
yuriiburov <burovyuriireact@gmail.com>
2022-01-11 16:30:15 +0200

    init

--- lesson04_state/task1/src/App.jsx
+++ lesson04_state/task1/src/App.jsx
@@ -4,8 +4,9 @@ import Counter from './Counter';
 const App = () => {
   return (
     <>
-      <Counter start={17} interval={1000} />
-      <Counter start={-50} interval={100} />
+      <Counter start={5} interval={1000} />
+      <Counter start={12} interval={1000} />
+      <Counter start={-21} interval={10} />
     </>
   );
 };
