0c7273995f1590a330a7d517819b73837e84767c
yuriiburov <burovyuriireact@gmail.com>
2022-01-11 14:25:07 +0200

    init

--- lesson03_components/task5/src/Profile.jsx
+++ lesson03_components/task5/src/Profile.jsx
@@ -1,14 +1,15 @@
 import React from 'react';
 import moment from 'moment';
+import './index.css';
 
 const Profile = ({ userData }) => {
   const { firstName, lastName, birthDate, birthPlace } = userData;
   const date = moment(new Date(birthDate)).format('DD MMM YY');
   return (
-    <>
+    <div className="profile">
       <div className="profile__name">{`${firstName} ${lastName} `}</div>
       <div className="profile__birth">{`Was born ${date} in ${birthPlace}`}</div>
-    </>
+    </div>
   );
 };
 
