5d5d55195e0721f312c7391f8729cb0409a5b3eb
yuriiburov <burovyuriireact@gmail.com>
2022-01-19 21:39:26 +0200

    init

--- lesson07_lists/task3/src/Transaction.jsx
+++ lesson07_lists/task3/src/Transaction.jsx
@@ -4,7 +4,7 @@ import moment from 'moment';
 const Transaction = ({ from, to, amount, rate, time }) => {
   return (
     <li className="transaction">
-      <span className="transaction__date">{moment(time).format('D MMM')}</span>
+      <span className="transaction__date">{moment(time).format('DD MMM')}</span>
       <span className="transaction__time">{moment(time).format('hh:mm')}</span>
       <span className="transaction__assets">
         {from} → {to}
--- lesson07_lists/task3/src/TransactionsList.jsx
+++ lesson07_lists/task3/src/TransactionsList.jsx
@@ -4,8 +4,8 @@ import Transaction from './Transaction';
 const TransactionsList = ({ transactions }) => {
   return (
     <ul className="transactions">
-      {transactions.slice().map((el) => (
-        <Transaction key={el.id} {...el} />
+      {transactions.slice().map((transaction) => (
+        <Transaction key={transaction.id} {...transaction} />
       ))}
     </ul>
   );
