65243ffbea658abd14d12e50e45f1d8f755a40f1
yuriiburov <burovyuriireact@gmail.com>
2022-01-22 18:04:47 +0200

    init

--- lesson08_lifecycle-methods/task4/src/index.js
+++ lesson08_lifecycle-methods/task4/src/index.js
@@ -3,4 +3,4 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import User from './User';
 
-ReactDOM.render(<User userId="yuriiburov" />, document.querySelector('#root'));
+ReactDOM.render(<User userId="facebook" />, document.querySelector('#root'));
