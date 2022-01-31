5bf46d68e165e6c7deacabc0cee7a2404153c86d
yuriiburov <burovyuriireact@gmail.com>
2022-01-31 23:14:34 +0200

    init

--- lesson11_update-component/03-HW/src/App.jsx
+++ lesson11_update-component/03-HW/src/App.jsx
@@ -1,30 +1,17 @@
 import React from 'react';
 import Expand from './Expand';
 
-class App extends React.Component {
-  state = {
-    isShow: false,
-  };
-
-  handleClick = () => {
-    this.setState({ isShow: !this.state.isShow });
-  };
-
-  render() {
-    return (
-      <div className="app">
-        <Expand
-          additionalContent={this.handleClick}
-          showContent={this.state.isShow}
-        >
-          <p>
-            Hooks are a new addition in React 16.8. They let you use state and
-            other React features without writing a class.
-          </p>
-        </Expand>
-      </div>
-    );
-  }
-}
+const App = () => {
+  return (
+    <div className="app">
+      <Expand>
+        <p>
+          Hooks are a new addition in React 16.8. They let you use state and
+          other React features without writing a class.
+        </p>
+      </Expand>
+    </div>
+  );
+};
 
 export default App;
--- lesson11_update-component/03-HW/src/Expand.jsx
+++ lesson11_update-component/03-HW/src/Expand.jsx
@@ -1,23 +1,28 @@
 import React from 'react';
 
 class Expand extends React.Component {
+  state = {
+    isShow: false,
+  };
+
+  handleClick = () => {
+    this.setState({ isShow: !this.state.isShow });
+  };
+
   render() {
     return (
       <div className="expand border">
         <div className="expand__header">
           <span className="expand__title">Some title</span>
-          <button
-            className="expand__toggle-btn"
-            onClick={this.props.additionalContent}
-          >
-            {this.props.showContent ? (
+          <button className="expand__toggle-btn" onClick={this.handleClick}>
+            {this.state.isShow ? (
               <i className="fas fa-chevron-up"></i>
             ) : (
               <i className="fas fa-chevron-down"></i>
             )}
           </button>
         </div>
-        {this.props.showContent ? (
+        {this.state.isShow ? (
           <div className="expand__content">{this.props.children}</div>
         ) : null}
       </div>
