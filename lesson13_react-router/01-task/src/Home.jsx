446f4c8568eb9fe0d262b1302799b1815b9c6288
yuriiburov <burovyuriireact@gmail.com>
2022-02-03 12:56:46 +0200

    init with some changes

--- lesson13_react-router/01-task/src/Home.jsx
+++ lesson13_react-router/01-task/src/Home.jsx
@@ -5,7 +5,7 @@ function Home() {
   return (
     <div className="page__content">
       <h1>🏠</h1>
-      <Link to="/products">To Products</Link>
+      <Link to="/products">To products</Link>
     </div>
   );
 }
