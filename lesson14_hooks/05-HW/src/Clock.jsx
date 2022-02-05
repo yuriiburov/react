b41d9412bc3bd739c113725b2ac4c2b34f8ceb25
yuriiburov <burovyuriireact@gmail.com>
2022-02-05 22:06:39 +0200

    init

--- lesson14_hooks/05-HW/src/Clock.jsx
+++ lesson14_hooks/05-HW/src/Clock.jsx
@@ -9,25 +9,22 @@ const getTimeWithOffset = (offset) => {
   );
 };
 
+const defaultTools = {
+  hour: 'numeric',
+  hour12: true,
+  minute: 'numeric',
+  second: 'numeric',
+};
+
 const Clock = ({ location, offset }) => {
   const [time, setTime] = useState(
-    getTimeWithOffset(offset).toLocaleTimeString('en-US', {
-      hour: 'numeric',
-      hour12: true,
-      minute: 'numeric',
-      second: 'numeric',
-    })
+    getTimeWithOffset(offset).toLocaleTimeString('en-US', defaultTools)
   );
 
   useEffect(() => {
     const interval = setInterval(() => {
       setTime(
-        getTimeWithOffset(offset).toLocaleTimeString('en-US', {
-          hour: 'numeric',
-          hour12: true,
-          minute: 'numeric',
-          second: 'numeric',
-        })
+        getTimeWithOffset(offset).toLocaleTimeString('en-US', defaultTools)
       );
     }, 1000);
     return () => {
@@ -42,61 +39,5 @@ const Clock = ({ location, offset }) => {
     </div>
   );
 };
-
-// class Clock extends React.Component {
-//   constructor(props) {
-//     super(props);
-
-//     this.state = {
-//       time: this.getTimeWithOffset(this.props.offset).toLocaleTimeString(
-//         'en-US',
-//         {
-//           hour: 'numeric',
-//           hour12: true,
-//           minute: 'numeric',
-//           second: 'numeric',
-//         }
-//       ),
-//     };
-//   }
-
-//   getTimeWithOffset = (offset) => {
-//     const currentTime = new Date();
-//     const utcOffset = currentTime.getTimezoneOffset() / 60;
-
-//     return new Date(
-//       currentTime.setHours(currentTime.getHours() + offset + utcOffset)
-//     );
-//   };
-
-//   componentDidMount() {
-//     this.interval = setInterval(() => {
-//       this.setState({
-//         time: this.getTimeWithOffset(this.props.offset).toLocaleTimeString(
-//           'en-US',
-//           {
-//             hour: 'numeric',
-//             hour12: true,
-//             minute: 'numeric',
-//             second: 'numeric',
-//           }
-//         ),
-//       });
-//     }, 1000);
-//   }
-
-//   componentWillUnmount() {
-//     clearInterval(this.interval);
-//   }
-
-//   render() {
-//     return (
-//       <div className="clock">
-//         <div className="clock__location">{this.props.location}</div>
-//         <div className="clock__time">{this.state.time}</div>
-//       </div>
-//     );
-//   }
-// }
 
 export default Clock;
