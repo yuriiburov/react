d8b7c71c3ed272cf78b46092334a7d7384ca95f2
yuriiburov <burovyuriireact@gmail.com>
2022-02-05 21:30:13 +0200

    init

---
+++ lesson14_hooks/04-HW/src/ConnectionStatus.jsx
@@ -0,0 +1,31 @@
+import React, { useEffect, useState } from 'react';
+
+const ConnectionStatus = () => {
+  const [status, setStatus] = useState('online');
+
+  useEffect(() => {
+    const handleOnline = () => {
+      setStatus('online');
+    };
+
+    const handleOffline = () => {
+      setStatus('offline');
+    };
+
+    window.addEventListener('online', handleOnline);
+    window.addEventListener('offline', handleOffline);
+
+    return () => {
+      window.removeEventListener('online', handleOnline);
+      window.removeEventListener('offline', handleOffline);
+    };
+  }, [status]);
+
+  return (
+    <div className={`status ${status === 'offline' ? 'status_offline' : null}`}>
+      {status === 'online' ? 'Online' : 'Offline'}
+    </div>
+  );
+};
+
+export default ConnectionStatus;
